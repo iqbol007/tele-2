8e3dd47d298cd42b19a594ee9655fb6a4cd65d26
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-05 05:42:06 +0000

    Deploy iqbol007/tele-2 to github.com/iqbol007/tele-2.git:gh-pages

---
+++ precache-manifest.e35e40a129ba2992c62c030d3b8d1221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf19d61f3f6c9609654f32328215f80",
+    "url": "/tele-2/index.html"
+  },
+  {
+    "revision": "b009e76dbcb7b785c092",
+    "url": "/tele-2/static/css/main.42437049.chunk.css"
+  },
+  {
+    "revision": "1812ff04a9f96dde04d1",
+    "url": "/tele-2/static/js/2.f8459bf0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tele-2/static/js/2.f8459bf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b009e76dbcb7b785c092",
+    "url": "/tele-2/static/js/main.992d8ed6.chunk.js"
+  },
+  {
+    "revision": "747174165facf1834eee",
+    "url": "/tele-2/static/js/runtime-main.5826684e.js"
+  }
+]);
