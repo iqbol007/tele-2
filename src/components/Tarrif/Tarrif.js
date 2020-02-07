c0f2474ef326e415de6123e284c73ab94080c480
iqbol <iqbol@localhost>
2020-02-07 05:33:37 +0300

    Fixes #4

--- src/components/Tarrif/Tarrif.js
+++ src/components/Tarrif/Tarrif.js
@@ -9,70 +9,70 @@ export default function Tarrif({ tele }) {
   const classic = <>Классический</>;
   const forDevices = <>Интернет для устройств</>;
   const withOutAmount = <>Без абонентской платы</>;
-  return (
-    <>
-      {!tele.withOutAmount ? (
-        <div className="main">
-          <div>
-            <div className="block-1">
-              <div className="title"> {tele.title}</div>
-              {tele.itsHit && <div className="image-hit"></div>}
-              {price}
-              {rub}
-              {tele.forDay ? day : moon}
+  if (!tele.withOutAmount) {
+    return (
+      <div className="main">
+        <div>
+          <div className="block-1">
+            <div className="title"> {tele.title}</div>
+            {tele.itsHit && <div className="image-hit"></div>}
+            {price}
+            {rub}
+            {tele.forDay ? day : moon}
+          </div>
+          <div className="block-2">
+            <div className="traffic-size">
+              {tele.traffic > 1 ? (
+                <>
+                  <b>{tele.traffic}</b>
+                  <span>ГБ</span>
+                </>
+              ) : (
+                <>
+                  {!tele.unlimInternet ? (
+                    <b>
+                      {tele.traffic * 1000}
+                      <span>МБ</span>
+                    </b>
+                  ) : (
+                    <div className="internet">БЕЗЛИМИТНЫЙ ИНТЕРНЕТ</div>
+                  )}
+                </>
+              )}
+              <br />
+              <span>
+                {tele.socialMediaInclude && <span>+безлимитные</span>}
+              </span>
             </div>
-            <div className="block-2">
-              <div className="traffic-size">
-                {tele.traffic > 1 ? (
-                  <>
-                    <b>{tele.traffic}</b>
-                    <span>ГБ</span>
-                  </>
-                ) : (
-                  <>
-                    {!tele.unlimInternet ? (
-                      <b>
-                        {tele.traffic * 1000}
-                        <span>МБ</span>
-                      </b>
-                    ) : (
-                      <div className="internet">БЕЗЛИМИТНЫЙ ИНТЕРНЕТ</div>
-                    )}
-                  </>
-                )}
-                <br />
-                <span>
-                  {tele.socialMediaInclude && <span>+безлимитные</span>}
-                </span>
-              </div>
-              {tele.socialMediaInclude && <div className="img-icons"></div>}
-              {tele.rowming && rowming}
+            {tele.socialMediaInclude && <div className="img-icons"></div>}
+            {tele.rowming && rowming}
+          </div>
+          <div className="block-3">
+            <div className="time">
+              {tele.time !== 0 && (
+                <>
+                  <b>{tele.time}</b>мин.
+                </>
+              )}
             </div>
-            <div className="block-3">
-              <div className="time">
-                {tele.time !== 0 && (
-                  <>
-                    <b>{tele.time}</b>мин.
-                  </>
-                )}
-              </div>
-              <div className="withOutLimit">
-                {tele.withOutLimit !== "" && tele.withOutLimit}
-              </div>
+            <div className="withOutLimit">
+              {tele.withOutLimit !== "" && tele.withOutLimit}
             </div>
           </div>
-          <div className="block-4">
-            {tele.sms !== 0 && <div className="sms">{tele.sms}SMS</div>}
-          </div>
         </div>
-      ) : (
-        <div className="withOutAmount">
-          {tele.forDevices && <b>{forDevices}</b>}
-          {tele.classic && <b>{classic}</b>}
-          <br />
-          {withOutAmount}
+        <div className="block-4">
+          {tele.sms !== 0 && <div className="sms">{tele.sms}SMS</div>}
         </div>
-      )}
-    </>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div className="withOutAmount">
+        {tele.forDevices && <b>{forDevices}</b>}
+        {tele.classic && <b>{classic}</b>}
+        <br />
+        {withOutAmount}
+      </div>
+    );
+  }
 }
